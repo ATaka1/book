5db1dfec67cadce0807ffb2122635dd13c815110
ATaka1 <a.takahashi13@gmail.com>
2018-04-25 14:07:30 -0700

    Initial commit

---
+++ chapter 2/ex.1.js
@@ -0,0 +1,13 @@
+// let star =  "#";
+//
+// while(star.length <= 7) {
+//     console.log(star);
+//     star += "#";
+//
+// }
+let hash = "#";
+for (let num = 1; num < 7; num++) {
+    console.log(hash);
+    hash += "#";
+}
+
