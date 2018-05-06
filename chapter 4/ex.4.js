07c579ae366e5f263d05634fa5f8e2f6b138fe31
Andrew Takahashi <a.takahashi13@gmail.com>
2018-05-05 19:49:07 -0700

    finish ex 4 last

--- chapter 4/ex.4.js
+++ chapter 4/ex.4.js
@@ -3,17 +3,29 @@ function deepEqual(x, y) {
         return true;
     }
 
-    if (typeof x === 'object' && x !== null && typeof y === 'object' && y !== null){
+    else if (typeof x === 'object' && x !== null && typeof y === 'object' && y !== null){
+        if( Object.keys(x).length !== Object.keys(y).length) {
+            return false;
+        }
         for (let prop in x) {
             if(y.hasOwnProperty(prop)) {
-                deepEqual(x[prop], y[prop]);
+                if ( !deepEqual(x[prop], y[prop]) ) {
+                    return false;
+                }
+            }
+            else {
+                return false;
             }
         }
+        return true;
     }
-    return true;
+    else {
+        return false;
+    }
+
 }
 
 let obj = {here: {is: "an"}, object: 2};
-// console.log(deepEqual(obj, obj));
+console.log(deepEqual(obj, obj));
 console.log(deepEqual(obj, {here: 1, object: 2}));
-// console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
